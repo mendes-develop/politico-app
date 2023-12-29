24c5abec3a32524081af81a0a507b6903c269440
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2023-12-29 00:53:03 -0500

    feat: read data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import './App.css';
 import { PerfilTile } from '../components/PerfilTile';
+import { useEffect, useState } from 'react';
 
-const img = 'https://ichef.bbci.co.uk/news/640/cpsprodpb/DBCE/production/_102107265_667060400.jpg';
+const URL_BASE = 'https://dadosabertos.camara.leg.br/api/v2/';
 
-const api_politicos = [
-	{ name: 'John', cep: 'Sao Paulo', partido: 'BRASIL', img: img },
-	{ name: 'Silvana', cep: 'Sao Paulo', partido: 'BRASIL', img: img },
-	{ name: 'Betty', cep: 'Sao Paulo', partido: 'BRASIL', img: img },
-];
-
-// MAP
-// method que permite criar novos arrays baseados num array existente
 function App() {
+	// Estado global do component
+	const [politicos, setPoliticos] = useState([]); // => [estado, altera estado]
+
+	async function fetchAndUpdate() {
+		//1. toda a logica de HTTP aqui dentro
+		//2. Atualizar o Estado
+		//3. renderizar o component
+		const resp = await fetch(URL_BASE + '/deputados');
+		const respostaTratada = await resp.json();
+		setPoliticos(respostaTratada.dados);
+	}
+
+	useEffect(() => {
+		fetchAndUpdate();
+	}, []);
+
 	return (
 		<>
 			<div>
-				{api_politicos.map((politico) => {
-					return (
-						<PerfilTile key={politico.name} name={politico.name} cep={politico.cep} img={politico.img} />
-					);
-				})}
+				{politicos.map((politico) => (
+					<PerfilTile key={politico.nome} name={politico.nome} cep={politico.siglaUf} img={politico.uri} />
+				))}
 			</div>
 		</>
 	);
